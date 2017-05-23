ccbdb96195398b35fa6a9e07b7af0e5af9b9c2b6
Radek Fryšták <geniv.radek@gmail.com>
2017-05-24 01:32:53 +0200

    added new code

---
+++ src/LatteDescriptionFilter.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Seo;
+
+use Latte\Runtime\FilterInfo;
+use Nette\Application\Application;
+use Nette\SmartObject;
+use Translator\Translator;
+
+
+/**
+ * Class LatteDescriptionFilter
+ *
+ * @author  geniv
+ * @package Seo
+ */
+class LatteDescriptionFilter
+{
+    use SmartObject;
+
+    /** @var Translator class */
+    private $translator;
+    /** @var Application current application */
+    private $application;
+
+
+    /**
+     * LatteDescriptionFilter constructor.
+     *
+     * @param Translator  $translator
+     * @param Application $application
+     */
+    public function __construct(Translator $translator, Application $application)
+    {
+        $this->translator = $translator;
+        $this->application = $application;
+    }
+
+
+    /**
+     * Magic call from template.
+     *
+     * @param FilterInfo $info
+     * @param            $string
+     * @return string
+     */
+    public function __invoke(FilterInfo $info, $string)
+    {
+        $presenter = $this->application->getPresenter();
+//        $this->model->insertSeoDescription($presenter, $string);
+
+        return 'TODO description';
+    }
+}
